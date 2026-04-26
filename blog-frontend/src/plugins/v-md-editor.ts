30ee8d25e8676a3c146ab435287f9a46686560cc
無以菱 <huangjing510@126.com>
2026-04-26 18:31:37 +0800

    perf: 精简Prism代码高亮语言包至常用6种语言

--- blog-frontend/src/plugins/v-md-editor.ts
+++ blog-frontend/src/plugins/v-md-editor.ts
@@ -6,21 +6,13 @@ import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
 import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
 import Prism from 'prismjs'
 
-// 引入代码高亮语言包
+// 引入代码高亮语言包（精简为常用的 6 个）
 import 'prismjs/components/prism-json'
 import 'prismjs/components/prism-bash'
-import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-go'
 import 'prismjs/components/prism-python'
-import 'prismjs/components/prism-java'
-import 'prismjs/components/prism-c'
-import 'prismjs/components/prism-cpp'
+import 'prismjs/components/prism-go'
 import 'prismjs/components/prism-sql'
 import 'prismjs/components/prism-yaml'
-import 'prismjs/components/prism-markdown'
-import 'prismjs/components/prism-css'
-import 'prismjs/components/prism-scss'
 
 // 配置配置项
 const config = {
